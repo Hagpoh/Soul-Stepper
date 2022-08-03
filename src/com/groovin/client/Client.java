6650c05ececae93b9a8c72190f377c2b5e6caf21
Meebly <hagpoh@gmail.com>
2022-08-03 14:36:08 -0700

    AB#931 added GUIHelper and CustomOutputStream

--- src/com/groovin/client/Client.java
+++ src/com/groovin/client/Client.java
@@ -1,9 +1,16 @@
 package com.groovin.client;
 
 import com.groovin.gameSetup.Game;
+import com.groovin.gui.MainGUI;
+
+import javax.swing.*;
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws InterruptedException {
+        MainGUI.createAndShowGUI();
         //initiating game class
         Game game = new Game();
         //calling play method from game class.
